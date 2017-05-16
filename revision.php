c948b1747459b8620c9b2f5a2c4c7b12780dbbed
cetecom <cetecom@LC08L5714>
2017-05-16 08:05:35 -0400

    trabajos sesiones PHP 4

--- revision.php
+++ revision.php
@@ -1,5 +1,17 @@
 <?php 
-var_dump($_SESSION)
+$PATH='lib/';
+
+include $PATH.'Conexion.php';
+include $PATH.'Usuario.php';
+
+$oUsr=new Usuario();
+
+$oUsr->nombre='nuevo';
+
+if($oUsr->VerificaUsuario())
+    echo "Existe";
+else
+    echo "No Existe";
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
