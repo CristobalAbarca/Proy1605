c0812347da36f7285cbd78299e12f99589ff4815
cetecom <cetecom@LC08L5714>
2017-05-16 08:30:24 -0400

    inicio de usuario y clave

--- index.php
+++ index.php
@@ -9,12 +9,18 @@ $_SESSION["saludo"]="Hola Mundo";
         <meta charset="UTF-8">
         <title></title>
     </head>
+
     <body>
        
         <a href="revision.php"> Revisi&oacute;n sesi&oacute;n</a>
+        <form>
+        <input type="password" nombre="clave">
+        </form>
         <?php 
         echo md5('#holamundo&');
         echo '<br>';
         ?>
+        
+         
     </body>
 </html>
